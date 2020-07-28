dbae66d64d3b4f95c91adf5093ce826e98718fca
goemon11 <ycfc91ryosogo11.10@gmail.com>
2020-07-28 14:04:40 +0900

    add-function-delete-show-edit

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -12,14 +12,26 @@ class MemosController < ApplicationController
   end
 
   def edit
+    @memo = Memo.find(params[:id])
   end
 
   def create
     memo = Memo.new(memo_params)
     memo.save!
-    redirect_to memos_url, notice: "メモ「#{memo.title}」を保存しました。"
+    redirect_to memos_url, notice: "メモ：タイトル「#{memo.title}」を保存しました。"
   end
 
+  def update
+    memo = Memo.find(params[:id])
+    memo.update!(memo_params)
+    redirect_to memos_url, notice: "メモ：タイトルy「#{memo.title}」を更新しました。"
+  end
+
+  def destroy
+    memo = Memo.find(params[:id])
+    memo.destroy
+    redirect_to memos_url, notice: "メモ：タイトル「#{memo.title}」を削除しました。"
+  end
   private
 
   def memo_params
